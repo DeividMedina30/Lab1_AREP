23eb8a76e8db8c4d58fdfbf26c85a8259d24a691
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-02-08 01:34:54 -0500

    Configurando el puerto que nos asigna Heroku.

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -2,16 +2,22 @@ package edu.escuelaing.arep;
 
 import static spark.Spark.*;
 public class SparkWebApp {
-
+	
+	
     public static void main(String[] args) {
-        port(getPort());
+    	port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
-
-    public static int getPort() {
-        if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
+    
+    /*
+     * 	Heroku asigna a su aplicación un nuevo puerto cada vez que la implementa
+     *  Debemos obtener este puerto y decirle a Spark que lo use
+     */
+    public	static int getHerokuAssignedPort() {
+    	ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; //returns default port if heroku-port isn't set
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
