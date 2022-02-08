f1cc9e2df038f8eb3a1d0320c3b2509a583c4a49
Deivid Medina <deivid.medina@mail.esccuelaing.edu.co>
2022-02-08 15:52:03 -0500

    Creando index.js

--- src/main/resources/paginaWeb/index.js
+++ src/main/resources/paginaWeb/index.js
@@ -1,4 +1,6 @@
 function capturarCelsius(){
     var celcius = document.getElementById("Celsius").value;
     console.log(celcius)
+    $("#etiquetaMostarCelsiusaFahrenheit").val(celcius);
+    console.log(celcius)
 }
