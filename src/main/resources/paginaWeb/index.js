8433958b9e09e817a31ced95a451331d200e695a
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-02-08 21:47:13 -0500

    Corrigiendo index.js

--- src/main/resources/paginaWeb/index.js
+++ src/main/resources/paginaWeb/index.js
@@ -1,14 +1,13 @@
-var celcius = document.getElementById("Celsius").value;
+
 function capturarCelsius(){
+	var celcius = document.getElementById("Celsius").value;
 	const url = 'https://areptemperatura.herokuapp.com/calcularTemperatura/Celsius'
     fetch(`${url}/${celcius}`)
 	.then(response => response.json())
 	.then(data => {
 		let celciusVer = document.getElementById('celsiusNew')
-		celciusVer.innerHTML = `<p>Los grados celsius en Fahrenheit es: ${data}</p>`
+		celciusVer.innerHTML = `<p>Los grados celsius: ${celcius} a Fahrenheit es: ${data}</p>`
 		console.log(data)
 	})
 	.catch(err=>console.log(err))
 }
-
-celcius.addEventListener('click',capturarCelsius,true)
