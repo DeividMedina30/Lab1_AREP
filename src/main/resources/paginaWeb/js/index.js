e7d64a89e3b4375d184ea69efdf06a47d14fd801
unknown <deivid.medina@labinfo.is.escuelaing.edu.co>
2022-02-09 10:22:26 -0500

    Modificando index.js

--- src/main/resources/paginaWeb/js/index.js
+++ src/main/resources/paginaWeb/js/index.js
@@ -6,18 +6,21 @@
 */
 
 //Función que me permite capturar los datos de grado Celsius para convertir a Fahrenheit.
-function capturarCelsius(){
-	var celcius = document.getElementById("Celsius").value;
-	const url = 'https://areptemperatura.herokuapp.com/calcularTemperatura/Celsius'
+
+
+
+var celcius = document.getElementById("Celsius").value;
+celcius.addEventListener('click', function(){
+    const url = 'https://areptemperatura.herokuapp.com/calcularTemperatura/Celsius'
     fetch(`${url}/${celcius}`)
-	.then(response => response.json())
-	.then(data => {
-		let celciusVer = document.getElementById('celsiusNew')
-		celciusVer.innerHTML = `<p>Los grados Celsius: ${celcius} a Fahrenheit es: ${data}</p>`
-		console.log(data)
-	})
-	.catch(err=>console.log(err))
-}
+    .then(response => response.json())
+    .then(data => {
+        let celciusVer = document.getElementById('celsiusNew')
+        celciusVer.innerHTML = `<p>Los grados Celsius: ${celcius} a Fahrenheit es: ${data}</p>`
+    	console.log(data)
+    })
+    .catch(err=>console.log(err))
+});
 
 //Función que me permite capturar los datos de grado Fahrenheit para convertir a Celsius.
 function capturarFahrenheit(){
