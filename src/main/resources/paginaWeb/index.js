a796e701cf1f5381a8dab0dfa38d944a9c945595
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2022-02-08 21:02:09 -0500

    Corrigiendo index.js

--- src/main/resources/paginaWeb/index.js
+++ src/main/resources/paginaWeb/index.js
@@ -1,6 +1,12 @@
 function capturarCelsius(){
+	const url = 'https://areptemperatura.herokuapp.com/calcularTemperatura/Celsius'
     var celcius = document.getElementById("Celsius").value;
-    console.log(celcius)
-    $("#etiquetaMostarCelsiusaFahrenheit").val(celcius);
-    console.log(celcius)
+    fetch('${url}/${celcius}')
+	.then(response => response.json())
+	.then(data => {
+		let celciusVer = document.getElementById('celsiusNew')
+		celciusVer.innerHTML = '<p>${data}</p>'
+		console.log(data)
+	})
+	.catch(err=>console.log(err))
 }
